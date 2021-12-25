5d8831c02ebd6017552ccc0198b8ea9b6d215bee
Wahidin Mahardika <code.nhistic@gmail.com>
2021-12-25 19:50:55 +0700

    page about

---
+++ src/pages/about.js
@@ -0,0 +1,58 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import Title from "../components/title"
+import ReactMarkdown from "react-markdown"
+import { GatsbyImage } from "gatsby-plugin-image";
+
+const About = ({
+  data: {
+    about: { nodes },
+  },
+ }) => {
+   const { title, stack, content, image } = nodes[0]
+
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <GatsbyImage image={image.localFile.childImageSharp.gatsbyImageData} className="about-img" title="avatar" />
+          <article className="about-text">
+            <Title title={title} />
+            <ReactMarkdown children={content} />
+            <div className="about-stack">
+              {stack.map((item) => {
+                return (
+                  <span key={item.id}>{item.name}</span>
+                )
+              })}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export const query = graphql`{
+  about: allStrapiAbout {
+    nodes {
+      stack {
+        id
+        name
+      }
+      title
+      content
+      image {
+        localFile {
+          childImageSharp{
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+export default About
